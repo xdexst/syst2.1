b8038bf382efea238a5419d45670168e656b14db
xdexst <rnoy10303@gmail.com>
2023-06-10 12:02:46 +0400

    2.1

---
+++ 2.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main () {
+int a;
+printf("lenght edge:");
+scanf("%d",&a);
+int b;
+b=a*a;
+int d;
+d=b*6;
+printf("Area of cube:%d cub. cm",d);
+return 0;
+}
